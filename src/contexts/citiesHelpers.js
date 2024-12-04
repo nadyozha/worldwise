d60c30ad4508ae4a6e8f9b94ad5ac395acf8e661
nadyozha <alexandrsnadyozha@gmail.com>
2024-12-04 18:53:55 +0100

    Last commit

--- src/contexts/citiesHelpers.js
+++ src/contexts/citiesHelpers.js
@@ -1,10 +1,36 @@
-export const BASE_URL = 'http://localhost:9000';
-// export const BASE_URL = '/.netlify/functions';
+// export const BASE_URL = 'http://localhost:9000';
+export const BASE_URL = '/.netlify/functions';
+
+export async function fetchCitiesData(dispatch) {
+	dispatch({ type: 'loading' });
+	try {
+		const res = await fetch(`${BASE_URL}/getCities`);
+		const data = await res.json();
+		dispatch({ type: 'cities/loaded', payload: data });
+	} catch {
+		dispatch({ type: 'rejected', payload: 'There was an error loading data' });
+	}
+}
+
+export async function addCity(dispatch, newCity) {
+	dispatch({ type: 'loading' });
+	try {
+		const res = await fetch(`${BASE_URL}/addCity`, {
+			method: 'POST',
+			body: JSON.stringify(newCity),
+			headers: { 'Content-Type': 'application/json' },
+		});
+		const data = await res.json();
+		dispatch({ type: 'city/created', payload: data });
+	} catch {
+		dispatch({ type: 'rejected', payload: 'There was an error creating data' });
+	}
+}
 
 // export async function fetchCitiesData(dispatch) {
 // 	dispatch({ type: 'loading' });
 // 	try {
-// 		const res = await fetch(`${BASE_URL}/getCities`);
+// 		const res = await fetch(`${BASE_URL}/cities`);
 // 		const data = await res.json();
 // 		dispatch({ type: 'cities/loaded', payload: data });
 // 	} catch {
@@ -15,7 +41,7 @@ export const BASE_URL = 'http://localhost:9000';
 // export async function addCity(dispatch, newCity) {
 // 	dispatch({ type: 'loading' });
 // 	try {
-// 		const res = await fetch(`${BASE_URL}/addCity`, {
+// 		const res = await fetch(`${BASE_URL}/cities`, {
 // 			method: 'POST',
 // 			body: JSON.stringify(newCity),
 // 			headers: { 'Content-Type': 'application/json' },
@@ -26,17 +52,6 @@ export const BASE_URL = 'http://localhost:9000';
 // 		dispatch({ type: 'rejected', payload: 'There was an error creating data' });
 // 	}
 // }
-
-export async function fetchCitiesData(dispatch) {
-	dispatch({ type: 'loading' });
-	try {
-		const res = await fetch(`${BASE_URL}/cities`);
-		const data = await res.json();
-		dispatch({ type: 'cities/loaded', payload: data });
-	} catch {
-		dispatch({ type: 'rejected', payload: 'There was an error loading data' });
-	}
-}
 
 export async function fetchCityById(dispatch, id, currentCityId) {
 	if (Number(id) === currentCityId) return;
@@ -51,20 +66,7 @@ export async function fetchCityById(dispatch, id, currentCityId) {
 	}
 }
 
-export async function addCity(dispatch, newCity) {
-	dispatch({ type: 'loading' });
-	try {
-		const res = await fetch(`${BASE_URL}/cities`, {
-			method: 'POST',
-			body: JSON.stringify(newCity),
-			headers: { 'Content-Type': 'application/json' },
-		});
-		const data = await res.json();
-		dispatch({ type: 'city/created', payload: data });
-	} catch {
-		dispatch({ type: 'rejected', payload: 'There was an error creating data' });
-	}
-}
+
 
 export async function removeCity(dispatch, id) {
 	dispatch({ type: 'loading' });
