b646a30efc789213d12e1998e65eaf423a89d049
nadyozha <alexandrsnadyozha@gmail.com>
2024-12-04 14:20:29 +0100

    map file changed

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,7 @@
 import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvent } from 'react-leaflet';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import { useCities } from '../Contexts/CitiesContext';
+import { useCities } from '../contexts/CitiesContext';
 import { useGeolocation } from '../hooks/useGeolocation';
 import { useUrlPosition } from '../hooks/useUrlPosition';
 import styles from './Map.module.css';
