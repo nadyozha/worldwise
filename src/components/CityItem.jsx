c097fa5ddb86b191b0178a05673323f6a36d2709
nadyozha <alexandrsnadyozha@gmail.com>
2025-01-28 23:26:43 -0800

    commit

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -3,33 +3,38 @@ import styles from './CityItem.module.css';
 import { Link } from 'react-router-dom';
 
 const formatDate = (date) =>
-    new Intl.DateTimeFormat("en", {
-        day: "numeric",
-        month: "long",
-        year: "numeric",
-        weekday: "long",
-    }).format(new Date(date));
+	new Intl.DateTimeFormat("en", {
+		day: "numeric",
+		month: "long",
+		year: "numeric",
+		weekday: "long",
+	}).format(new Date(date));
 
 export default function CityItem({ city }) {
-    const { currentCity, deleteCity } = useCities();
-    const { cityName, emoji, date, id, position } = city;
-    const { lat, lng } = position;
+	const { currentCity, deleteCity } = useCities();
+	const { cityName, emoji, date, id, position } = city;
+	const { lat, lng } = position;
 
-    function handleClick(e) {
-        e.preventDefault();
-        deleteCity(id);
-    }
+	function handleClick(e) {
+		e.preventDefault();
+		deleteCity(id);
+	}
 
-    return (
-        <li>
-            <Link to={`${id}?lat=${lat}&lng=${lng}`} className={`${styles.cityItem} ${currentCity.id === id ? styles['cityItem--active'] : ''}`}>
-                <span className={styles.emoji}>{emoji}</span>
-                <h3 className={styles.name}>{cityName}</h3>
-                <time className={styles.date}>{formatDate(date)}</time>
-                <button className={styles.deleteBtn} onClick={handleClick}>&times;</button>
-            </Link>
-        </li>
-    )
+	if (!city.position) {
+		// показываем заглушку вместо lat/lng
+		return <div>{city.cityName} (No coordinates)</div>;
+	}
+
+	return (
+		<li>
+			<Link to={`${id}?lat=${lat}&lng=${lng}`} className={`${styles.cityItem} ${currentCity.id === id ? styles['cityItem--active'] : ''}`}>
+				<span className={styles.emoji}>{emoji}</span>
+				<h3 className={styles.name}>{cityName}</h3>
+				<time className={styles.date}>{formatDate(date)}</time>
+				<button className={styles.deleteBtn} onClick={handleClick}>&times;</button>
+			</Link>
+		</li>
+	)
 }
 
 
