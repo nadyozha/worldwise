9e2b22d87fdfd571562981a5828f94aa853ee73c
nadyozha <alexandrsnadyozha@gmail.com>
2025-02-16 15:23:51 -0800

    commit

--- src/components/ErrorFallback.jsx
+++ src/components/ErrorFallback.jsx
@@ -1,9 +1,9 @@
 function ErrorFallback({ error, resetErrorBoundary }) {
 	return (
-		<div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-			<h1>Something went wrong 🧐</h1>
+		<div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh', gap: '1rem' }}>
+			<h1 style={{ color: 'red' }}>Something went wrong 🧐</h1>
 			<p>{error.message}</p>
-			<button onClick={resetErrorBoundary}>Try again</button>
+			<button style={{ padding: '1rem', backgroundColor: 'green', color: 'white' }} onClick={resetErrorBoundary}>Try again</button>
 		</div>
 	)
 }
