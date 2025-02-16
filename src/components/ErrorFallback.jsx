ed63f68eb2bf9c34ced9631810829189529ad8a9
nadyozha <alexandrsnadyozha@gmail.com>
2025-02-16 15:20:04 -0800

    EB styles addede

--- src/components/ErrorFallback.jsx
+++ src/components/ErrorFallback.jsx
@@ -1,6 +1,6 @@
 function ErrorFallback({ error, resetErrorBoundary }) {
 	return (
-		<div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+		<div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
 			<h1>Something went wrong 🧐</h1>
 			<p>{error.message}</p>
 			<button onClick={resetErrorBoundary}>Try again</button>
